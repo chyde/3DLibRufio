f68b7d04b82a2c4d986a9957b11175ed8f29efa2
chyde <hyde.wsu@gmail.com>
2015-09-15 10:05:56 -0700

    building out the new api

--- r2/script/hello.js
+++ r2/script/hello.js
@@ -2,27 +2,23 @@
 
 
 window.onload = function() {
-// Matter.js module aliases
+	// Matter.js module aliases
 	var Engine = Matter.Engine,
-	World = Matter.World,
-	Bodies = Matter.Bodies;
+		World = Matter.World,
+		Bodies = Matter.Bodies;
 
 	// create a Matter.js engine
-	var engine = Engine.create();
+	var engine = Engine.create(document.body);
 
 	// create two boxes and a ground
 	var boxA = Bodies.rectangle(400, 200, 80, 80);
-	var boxB = Bodies.rectangle(450, 50, 80, 120);
-	var ground = Bodies.rectangle(400, 610, 1000, 60, { isStatic: true });
+	var boxB = Bodies.rectangle(450, 50, 80, 80);
+	var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 	// add all of the bodies to the world
 	World.add(engine.world, [boxA, boxB, ground]);
 
 	// run the engine
-	//Engine.run(engine);
-	var f = function(){	for(var i = 0; i < 10; i++){
-		console.log(boxA.position);
-	}};
-	setTimeout(f,0);
+	Engine.run(engine);
 
 };
--- r2/script/r2.js
+++ r2/script/r2.js
@@ -6,6 +6,7 @@
 // All Rights Reserved 2015
 
 var r2 = r2 || {};
+
 const r2.debug = true;
 
 r2.log = function(module, content){
@@ -19,4 +20,83 @@ r2.init = function(){
 };
 
 
+r2.Debug_Display_Mode = {
+    NONE: "none",
+    NO_PROJECTILES: "no projectiles",
+    CREATURES: "creatures",
+    ALL: "all"
+};
 
+r2.DEBUG = true;
+r2.dbDisplay = r2.Debug_Display_Mode.ALL;
+
+r2.MyUserData = function(obj3D, matterBody) {
+    
+    this.obj3D = obj3D;
+    this.body = matterBody;
+
+    this.forceModifier
+
+    this.update = function() {
+    	//update "time"
+    	this.frames += 1;
+        //consider modifiers
+        this.customModifiers(); //run childClass's modifications (could be a menu pause or anything)
+        //apply modifications
+        //this.positionModifier.add(this.position).add(this.velocity).add(this.velocityModifier);
+        //this.obj3D.position.copy(this.positionModifier);
+        this.velocity.add(this.velocityModifier);
+        this.position.add(this.velocity).add(this.positionModifier);
+        this.obj3D.position.copy(this.position);
+        //only the rendered position takes into account Z
+        this.obj3D.position.z = this.zIndex;
+        //clear mods
+        this.velocityModifier.set(0, 0, 0);
+        this.positionModifier.set(0, 0, 0);
+        //TODO: This should happen last in the render loop
+        if(r2.DEBUG ) this.updateDebug();  //&& rufio.dbDisplay != rufio.Debug_Display_Mode.NONE
+    };
+
+    // To be set by child classes:
+    //  For modification that happens dynamically in child classes
+    this.customModifiers = function() {};
+
+    this.control = function(){};
+
+    this.mouseOver = function() {};
+
+    this.handleCollision = function(collidingObject) {};
+
+    //  May be custom for individual objects
+    this.constructDebugText = function(){
+        return "" + this.position.x.toFixed(2) +", " + this.position.y.toFixed(2) + "<br>" + this.orientation.toFixed(2);
+    }
+
+    //  Should not be modified.  Displays debug info.
+    this.updateDebug = function(){
+        //determine position
+        var y = -(this.position.y)/pixelsToUnits+(window.innerHeight/2);
+        var x = (this.position.x)/pixelsToUnits+(window.innerWidth/2)+15;
+
+
+        if(r2.dbDisplay == r2.Debug_Display_Mode.NONE){
+            this.debugDiv.style.display = "none";
+        }
+        else{
+            this.debugDiv.style.display = "inline";
+            this.debugDiv.innerHTML = this.constructDebugText();
+            this.debugDiv.style.top = y + 'px';
+            this.debugDiv.style.left = x + 'px';
+        }
+        //only display debug info *inside* the browser window.  (this will avoid scroll bars)
+        if(y < window.innerHeight*.2  || y > window.innerHeight*.8
+            || x < window.innerWidth*.2  || x > window.innerWidth*.8){
+            this.debugDiv.style.display = "none";
+        }
+    }
+
+    //  TODO: on create: debug info
+    if(r2.DEBUG){
+        //this.debugDiv = rufio.createDebugText(this.obj3D.id);// Release version will never reach this
+    }
+};
